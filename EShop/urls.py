953ef6f28a948e5c077d917a438e8c39698f346a
[Mohsen Ahmadi] <mohsenahmadi2003@yahoo.com>
2023-12-13 17:56:14 +0330

    add contact module url

--- EShop/urls.py
+++ EShop/urls.py
@@ -4,6 +4,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('home_module.urls')),
+    path('contact', include('contact_module.urls')),
     path('admin/', admin.site.urls),
     path('products/', include('product_module.urls')),
 ]
