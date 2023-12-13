184da55d9f7b52969845ce66359a34515b1a8953
[Mohsen Ahmadi] <mohsenahmadi2003@yahoo.com>
2023-12-13 18:08:17 +0330

    config contact module

--- contact_module/apps.py
+++ contact_module/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class ContactModuleConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'contact_module'
+    verbose_name = 'ماژول تماس با ما'
