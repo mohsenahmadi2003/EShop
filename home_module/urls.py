1fe35eb7ecd077c15a7382c47c7c9e1ed09eb6a6
[Mohsen Ahmadi] <mohsenahmadi2003@yahoo.com>
2023-12-16 17:38:04 +0330

    add name for index_page

--- home_module/urls.py
+++ home_module/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index_page),
+    path('', views.index_page, name='home_page'),
     path('contact-us', views.contact_page),
     # path('site-header', views.site_header_partial, name='site_header_partial')
 ]
