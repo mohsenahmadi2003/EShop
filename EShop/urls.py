7caf7acb7c357fb2c5ed9e9d75bc55499cb318b3
[Mohsen Ahmadi] <mohsenahmadi2003@yahoo.com>
2023-12-16 17:37:11 +0330

    Fix url contact

--- EShop/urls.py
+++ EShop/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('home_module.urls')),
-    path('contact', include('contact_module.urls')),
+    path('contact/', include('contact_module.urls')),
     path('admin/', admin.site.urls),
     path('products/', include('product_module.urls')),
 ]
