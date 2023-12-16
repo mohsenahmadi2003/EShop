8d7a148b14e19b1eb5e792d88d8f90bb8258e83a
[Mohsen Ahmadi] <mohsenahmadi2003@yahoo.com>
2023-12-16 17:35:41 +0330

    update models

--- contact_module/models.py
+++ contact_module/models.py
@@ -10,7 +10,7 @@ class ContactUs(models.Model):
     message = models.TextField(verbose_name='متن تماس با ما')
     created_date = models.DateTimeField(verbose_name='تاریخ ایجاد', auto_now_add=True)
     response = models.TextField(verbose_name='متن پاسخ تماس با ما', null=True, blank=True)
-    is_read_by_admin = models.BooleanField(verbose_name='خوانده شده توسط ادمین')
+    is_read_by_admin = models.BooleanField(verbose_name='خوانده شده توسط ادمین', default=False)
 
     class Meta:
         verbose_name = 'تماس با ما'
