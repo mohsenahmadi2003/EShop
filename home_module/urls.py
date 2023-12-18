7c7ccbae9022cff3d58cec5bcdd40da1a40c31c4
[Mohsen Ahmadi] <mohsenahmadi2003@yahoo.com>
2023-12-18 05:49:07 +0330

    update cbv url

--- home_module/urls.py
+++ home_module/urls.py
@@ -2,7 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index_page, name='home_page'),
-    path('contact-us', views.contact_page),
-    # path('site-header', views.site_header_partial, name='site_header_partial')
+    path('', views.HomeView.as_view(), name='home_page'),
 ]
