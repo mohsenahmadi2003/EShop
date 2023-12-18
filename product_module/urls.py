d6691ab583efeb9aa510a3fe7e46b47620bf60d2
[Mohsen Ahmadi] <mohsenahmadi2003@yahoo.com>
2023-12-18 05:47:28 +0330

    update cbv url

--- product_module/urls.py
+++ product_module/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.product_list, name='product-list'),
-    path('<slug:slug>', views.product_detail, name='product-detail'),
+    path('', views.ProductListView.as_view(), name='product-list'),
+    path('<slug:slug>', views.ProductDetailView.as_view(), name='product-detail'),
 ]
