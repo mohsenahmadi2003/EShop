84f7f7dd1b6c5286370076c474dc5cff37ad4804
[Mohsen Ahmadi] <mohsenahmadi2003@yahoo.com>
2023-12-19 10:42:49 +0330

    update ContactUsView [use form view]

--- contact_module/views.py
+++ contact_module/views.py
@@ -1,21 +1,14 @@
 from django.shortcuts import render, redirect
 from django.views import View
 from .forms import ContactUsModelForm
+from django.views.generic.edit import FormView
 
 
-class ContactUsView(View):
-    def get(self, request):
-        contact_form = ContactUsModelForm()
-        return render(request, 'contact_module/contact_us_page.html', {
-            'contact_form': contact_form
-        })
+class ContactUsView(FormView):
+    template_name = 'contact_module/contact_us_page.html'
+    form_class = ContactUsModelForm
+    success_url = '/contact-us/'
 
-    def post(self, request):
-        contact_form = ContactUsModelForm(request.POST)
-        if contact_form.is_valid():
-            contact_form.save()
-            return redirect('home_page')
-
-        return render(request, 'contact_module/contact_us_page.html', {
-            'contact_form': contact_form
-        })
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
