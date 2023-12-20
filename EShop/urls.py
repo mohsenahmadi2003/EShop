f9523937fe9f36e5834bcb613085a01b91ce9e5c
[Mohsen Ahmadi] <mohsenahmadi2003@yahoo.com>
2023-12-20 22:09:16 +0330

    add media url

--- EShop/urls.py
+++ EShop/urls.py
@@ -1,10 +1,13 @@
-
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('', include('home_module.urls')),
     path('contact/', include('contact_module.urls')),
-    path('admin/', admin.site.urls),
     path('products/', include('product_module.urls')),
+    path('admin/', admin.site.urls),
 ]
+
+urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
