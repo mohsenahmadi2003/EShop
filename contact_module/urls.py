759d8f4a902d5101a40aac596e29e8a8c4bcef5a
[Mohsen Ahmadi] <mohsenahmadi2003@yahoo.com>
2023-12-20 21:44:05 +0330

    add ProfilesView and CreateProfileView urls

--- contact_module/urls.py
+++ contact_module/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.ContactUsView.as_view(), name='contact_us_page'),
+    path('create-profile/', views.CreateProfileView.as_view(), name='create_profile_page'),
+    path('profiles/', views.ProfilesView.as_view(), name='profiles_page'),
 ]
