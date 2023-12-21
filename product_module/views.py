119fec003db06fc2521f15ff9b0942ec4276d327
[Mohsen Ahmadi] <mohsenahmadi2003@yahoo.com>
2023-12-21 22:53:16 +0330

    create AddProductFavorite view

--- product_module/views.py
+++ product_module/views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import render, get_object_or_404
-from django.views.generic.base import TemplateView
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views.generic.base import TemplateView, View
 from django.views.generic import ListView, DetailView
-
 from .models import Product
 
 
@@ -10,12 +9,15 @@ class ProductListView(ListView):
     model = Product
     context_object_name = 'products'
 
-    def get_queryset(self):
-        base_query = super(ProductListView, self).get_queryset()
-        data = base_query.filter(is_active=True)
-        return data
-
 
 class ProductDetailView(DetailView):
     template_name = 'product_module/product_detail.html'
     model = Product
+
+
+class AddProductFavorite(View):
+    def post(self, request):
+        product_id = request.POST["product_id"]
+        product = Product.objects.get(pk=product_id)
+        request.session["product_favorite"] = product_id
+        return redirect(product.get_absolute_url())
