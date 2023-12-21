bba5a309853896b4381d9e0b3a97c187948dae88
[Mohsen Ahmadi] <mohsenahmadi2003@yahoo.com>
2023-12-21 22:53:36 +0330

    add ProductFavorite url

--- product_module/urls.py
+++ product_module/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.ProductListView.as_view(), name='product-list'),
+    path('product-favorite', views.AddProductFavorite.as_view(), name='product-favorite'),
     path('<slug:slug>', views.ProductDetailView.as_view(), name='product-detail'),
 ]
