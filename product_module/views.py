6050dad9dc4e35bf2b19703b71e6973e56b6c4d3
[Mohsen Ahmadi] <mohsenahmadi2003@yahoo.com>
2023-12-23 10:27:09 +0330

    update ProductListView [add ordering and paginate]

--- product_module/views.py
+++ product_module/views.py
@@ -8,7 +8,8 @@ class ProductListView(ListView):
     template_name = 'product_module/product_list.html'
     model = Product
     context_object_name = 'products'
-
+    ordering = ['-price']
+    paginate_by = 1
 
 class ProductDetailView(DetailView):
     template_name = 'product_module/product_detail.html'
