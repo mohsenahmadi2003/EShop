489996a6fac77f5e97d46390430239c7a993c363
[Mohsen Ahmadi] <mohsenahmadi2003@yahoo.com>
2023-12-24 23:30:26 +0330

    add post method for RegisterView

--- account_module/views.py
+++ account_module/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import get_user_model
+
 from django.urls import reverse
 from django.views import View
 from .models import User
@@ -12,6 +13,31 @@ from account_module.forms import RegisterForm, LoginForm
 class RegisterView(View):
     def get(self, request):
         register_form = RegisterForm()
+        context = {
+            'register_form': register_form
+        }
+
+        return render(request, 'account_module/register.html', context)
+
+    def post(self, request):
+        register_form = RegisterForm(request.POST)
+        if register_form.is_valid():
+            user_email = register_form.cleaned_data.get('email')
+            user_password = register_form.cleaned_data.get('password')
+            user: bool = User.objects.filter(email__iexact=user_email).exists()
+            if user:
+                register_form.add_error('email', 'ایمیل وارد شده تکراری می باشد')
+            else:
+                new_user = User(
+                    email=user_email,
+                    email_active_code=get_random_string(72),
+                    is_active=False,
+                    username=user_email)
+                new_user.set_password(user_password)
+                new_user.save()
+                # todo: send email active code
+                return redirect(reverse('login_page'))
+
         context = {
             'register_form': register_form
         }
