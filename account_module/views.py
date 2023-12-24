bd248582c232fcb15a1b8207b36ccdfe65282ef5
[Mohsen Ahmadi] <mohsenahmadi2003@yahoo.com>
2023-12-24 23:29:36 +0330

    create RegisterView with get method

--- account_module/views.py
+++ account_module/views.py
@@ -1,1 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from django.views import View
+from .models import User
+from django.utils.crypto import get_random_string
+from django.http import Http404
+
+from account_module.forms import RegisterForm, LoginForm
+
+
+class RegisterView(View):
+    def get(self, request):
+        register_form = RegisterForm()
+        context = {
+            'register_form': register_form
+        }
+
+        return render(request, 'account_module/register.html', context)
