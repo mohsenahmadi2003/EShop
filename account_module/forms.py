510ea0a3614d03180a03677606b5db6e31aefae2
[Mohsen Ahmadi] <mohsenahmadi2003@yahoo.com>
2023-12-24 23:32:10 +0330

    create RegisterForm

---
+++ account_module/forms.py
@@ -0,0 +1,28 @@
+from django import forms
+from django.core import validators
+from django.core.exceptions import ValidationError
+
+
+class RegisterForm(forms.Form):
+    email = forms.EmailField(
+        label='ایمیل',
+        widget=forms.EmailInput(),
+        validators=[
+            validators.MaxLengthValidator(100),
+            validators.EmailValidator,
+        ]
+    )
+    password = forms.CharField(
+        label='کلمه عبور',
+        widget=forms.PasswordInput(),
+        validators=[
+            validators.MaxLengthValidator(100),
+        ]
+    )
+    confirm_password = forms.CharField(
+        label='تکرار کلمه عبور',
+        widget=forms.PasswordInput(),
+        validators=[
+            validators.MaxLengthValidator(100),
+        ]
+    )
