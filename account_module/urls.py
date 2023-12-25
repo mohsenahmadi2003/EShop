972a1987f5738c0b681692972e73f33a7041cb7e
[Mohsen Ahmadi] <mohsenahmadi2003@yahoo.com>
2023-12-25 10:47:33 +0330

    add forget, reset password urls

--- account_module/urls.py
+++ account_module/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('register', views.RegisterView.as_view(), name='register_page'),
     path('login', views.LoginView.as_view(), name='login_page'),
+    path('forget-pass', views.ForgetPassword.as_view(), name='forget_password_page'),
+    path('reset-pass/<active_code>', views.ResetPassword.as_view(), name='reset_password_page'),
     path('activate-account/<email_active_code>', views.ActivateAccountView.as_view(), name='activate_account'),
 ]
