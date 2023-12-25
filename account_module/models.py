43f450da5cea31fb62a2c1c0065753fdf2caf303
[Mohsen Ahmadi] <mohsenahmadi2003@yahoo.com>
2023-12-25 10:19:30 +0330

    update User model [add avatar]

--- account_module/models.py
+++ account_module/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class User(AbstractUser):
-    mobile = models.CharField(max_length=20, verbose_name='تلفن همراه')
+    avatar = models.CharField(max_length=20, verbose_name='تصویر آواتار', null=True, blank=True)
     email_active_code = models.CharField(max_length=100, verbose_name='کد فعالسازی ایمیل')
 
     class Meta:
