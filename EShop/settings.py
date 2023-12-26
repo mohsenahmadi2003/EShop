469532843a37428dd0d391e133548bc123dd583e
[Mohsen Ahmadi] <mohsenahmadi2003@yahoo.com>
2023-12-26 20:02:05 +0330

    [update]: add email config

--- EShop/settings.py
+++ EShop/settings.py
@@ -91,3 +91,11 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 SESSION_COOKIE_AGE = 1814400
+
+# Email
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_PORT = 587
