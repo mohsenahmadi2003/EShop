f794fb09db0fafb5ff97dc2e0f1a9153b943d080
[Mohsen Ahmadi] <mohsenahmadi2003@yahoo.com>
2023-12-27 20:35:25 +0330

    [update]: add LogoutView url

--- account_module/urls.py
+++ account_module/urls.py
@@ -4,7 +4,8 @@ from . import views
 urlpatterns = [
     path('register', views.RegisterView.as_view(), name='register_page'),
     path('login', views.LoginView.as_view(), name='login_page'),
-    path('forget-pass', views.ForgetPassword.as_view(), name='forget_password_page'),
-    path('reset-pass/<active_code>', views.ResetPassword.as_view(), name='reset_password_page'),
+    path('logout', views.LogoutView.as_view(), name='logout_page'),
+    path('forget-pass', views.ForgetPasswordView.as_view(), name='forget_password_page'),
+    path('reset-pass/<active_code>', views.ResetPasswordView.as_view(), name='reset_password_page'),
     path('activate-account/<email_active_code>', views.ActivateAccountView.as_view(), name='activate_account'),
 ]
