59603a8ed28dbe794050e4e4ac41ef8ec5249789
[Mohsen Ahmadi] <mohsenahmadi2003@yahoo.com>
2023-12-27 21:09:39 +0330

    [create SiteSetting]: create SiteSetting model

--- site_module/models.py
+++ site_module/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class SiteSetting(models.Model):
+    site_name = models.CharField(max_length=200, verbose_name='نام سایت')
+    site_url = models.CharField(max_length=200, verbose_name='دامنه سایت')
+    address = models.CharField(max_length=200, verbose_name='آدرس')
+    phone = models.CharField(max_length=200, null=True, blank=True, verbose_name='تلفن')
+    fax = models.CharField(max_length=200, null=True, blank=True, verbose_name='فکس')
+    email = models.CharField(max_length=200, null=True, blank=True, verbose_name='ایمیل')
+    copy_right = models.TextField(verbose_name='متن کپی رایت سایت')
+    about_us_text = models.TextField(verbose_name='متن درباره ما سایت')
+    site_logo = models.ImageField(upload_to='images/site-setting/', verbose_name='لوگو سایت')
+    is_main_setting = models.BooleanField(verbose_name='تنظیمات اصلی')
+
+    class Meta:
+        verbose_name = 'تنظیمات سایت'
+        verbose_name_plural = 'تنظیمات'
+
+    def __str__(self):
+        return self.site_name
