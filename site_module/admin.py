8ffb2fd6a876e3a2e8fd3dfd14bdd67798d81d4f
[Mohsen Ahmadi] <mohsenahmadi2003@yahoo.com>
2023-12-28 08:07:06 +0330

    [update]: register models in the admin panel

--- site_module/admin.py
+++ site_module/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+class FooterLinkAdmin(admin.ModelAdmin):
+    list_display = ['title', 'url']
+
+
+admin.site.register(models.SiteSetting)
+admin.site.register(models.FooterLinkBox)
+admin.site.register(models.FooterLink, FooterLinkAdmin)
