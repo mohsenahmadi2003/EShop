19e549b9fb026b228715ac87b9132f111dba9bd2
[Mohsen Ahmadi] <mohsenahmadi2003@yahoo.com>
2023-12-29 07:28:05 +0330

    [update]: add slider admin config

--- site_module/admin.py
+++ site_module/admin.py
@@ -6,6 +6,11 @@ class FooterLinkAdmin(admin.ModelAdmin):
     list_display = ['title', 'url']
 
 
+class SliderAdmin(admin.ModelAdmin):
+    list_display = ['title', 'url', 'is_active']
+    list_editable = ['url', 'is_active']
+
+
 admin.site.register(models.SiteSetting)
 admin.site.register(models.FooterLinkBox)
 admin.site.register(models.Slider)
