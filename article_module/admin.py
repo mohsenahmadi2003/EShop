bc19a1eba90a301fe746ad5898d096d1f5c97f97
[Mohsen Ahmadi] <mohsenahmadi2003@yahoo.com>
2023-12-30 10:23:46 +0330

    [update]: add article category admin register

--- article_module/admin.py
+++ article_module/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from . import models
 
-admin.site.register(models.ArticleCategory)
+
+class ArticleCategoryAdmin(admin.ModelAdmin):
+    list_display = ['title', 'url_title', 'parent', 'is_active']
+    list_editable = ['url_title', 'parent', 'is_active']
+
+
+admin.site.register(models.ArticleCategory, ArticleCategoryAdmin)
 admin.site.register(models.Article)
