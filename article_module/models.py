2ccf42706bfb84d880f89722cf711930d1d09555
[Mohsen Ahmadi] <mohsenahmadi2003@yahoo.com>
2023-12-30 10:20:11 +0330

    [create]: add article model

--- article_module/models.py
+++ article_module/models.py
@@ -14,3 +14,20 @@ class ArticleCategory(models.Model):
     class Meta:
         verbose_name = 'دسته بندی مقاله'
         verbose_name_plural = 'دسته بندی های مقاله'
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=300, verbose_name='عنوان مقاله')
+    slug = models.SlugField(max_length=400, db_index=True, allow_unicode=True, verbose_name='عنوان در url')
+    image = models.ImageField(upload_to='images/articles', verbose_name='تصویر مقاله')
+    short_description = models.TextField(verbose_name='توضیحات کوتاه')
+    text = models.TextField(verbose_name='متن مقاله')
+    is_active = models.BooleanField(default=True, verbose_name='فعال / غیرفعال')
+    selected_categories = models.ManyToManyField(ArticleCategory, verbose_name='دسته بندی ها')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'مقاله'
+        verbose_name_plural = 'مقالات'
