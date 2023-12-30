a86f5cca2022d0cd81fc1cb0fe413aa6cf9c9500
[Mohsen Ahmadi] <mohsenahmadi2003@yahoo.com>
2023-12-30 10:20:51 +0330

    [update]: add verbose name

--- article_module/apps.py
+++ article_module/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class ArticleModuleConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'article_module'
+    verbose_name = 'ماژول مقالات'
