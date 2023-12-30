e4eb69dbbed14dca74059113fdba1ecba8e4fe74
[Mohsen Ahmadi] <mohsenahmadi2003@yahoo.com>
2023-12-30 10:19:53 +0330

    [create]: add article category

--- article_module/models.py
+++ article_module/models.py
@@ -1,1 +1,16 @@
 from django.db import models
+
+
+class ArticleCategory(models.Model):
+    parent = models.ForeignKey('ArticleCategory', null=True, blank=True, on_delete=models.CASCADE,
+                               verbose_name='دسته بندی والد')
+    title = models.CharField(max_length=200, verbose_name='عنوان دسته بندی')
+    url_title = models.CharField(max_length=200, unique=True, verbose_name='عنوان در url')
+    is_active = models.BooleanField(default=True, verbose_name='فعال / غیرفعال')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'دسته بندی مقاله'
+        verbose_name_plural = 'دسته بندی های مقاله'
