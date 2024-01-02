f1041b3f1c33eab6d5264078ce9b4a70b530d2d7
[Mohsen Ahmadi] <mohsenahmadi2003@yahoo.com>
2024-01-02 22:02:44 +0330

    [update]: change context date for articles list view

--- article_module/views.py
+++ article_module/views.py
@@ -1,8 +1,14 @@
 from django.views.generic.list import ListView
 from article_module.models import Article
+from jalali_date import datetime2jalali, date2jalali
 
 
 class ArticlesListView(ListView):
     model = Article
     paginate_by = 5
     template_name = 'article_module/articles_page.html'
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(ArticlesListView, self).get_context_data(*args, **kwargs)
+        context['date'] = date2jalali(self.request.user.date_joined)
+        return context
