38c2438f09b7fed5d94c14558c4a6ecd3903351c
[Mohsen Ahmadi] <mohsenahmadi2003@yahoo.com>
2024-01-03 12:56:39 +0330

    [feat]: add cut template tag

--- polls/templatetags/poll_extras.py
+++ polls/templatetags/poll_extras.py
@@ -7,3 +7,9 @@ register = template.Library()
 @register.filter(name='show_jalali_date')
 def show_jalali_date(value):
     return date2jalali(value)
+
+
+@register.filter(name='cut')
+def cut(value, arg):
+    """Removes all values of arg from the given string"""
+    return value.replace(arg, '')
