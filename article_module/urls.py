21bc1c9ca69d481682d698abb1fd72ba092e6afa
[Mohsen Ahmadi] <mohsenahmadi2003@yahoo.com>
2024-01-04 23:33:46 +0330

    [update]: add url for article detail view

--- article_module/urls.py
+++ article_module/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.ArticlesListView.as_view(), name='articles_list')
+    path('', views.ArticlesListView.as_view(), name='articles_list'),
+    path('cat/<str:category>', views.ArticlesListView.as_view(), name='articles_by_category_list'),
+    path('<pk>/', views.ArticleDetailView.as_view(), name='articles_detail'),
 ]
