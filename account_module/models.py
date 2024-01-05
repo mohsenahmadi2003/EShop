a807632ee0dc101ce8a340f1edb0165aa429f486
[Mohsen Ahmadi] <mohsenahmadi2003@yahoo.com>
2024-01-05 23:20:34 +0330

    [update]: add about user filed and change avatar field

--- account_module/models.py
+++ account_module/models.py
@@ -3,15 +3,16 @@ from django.db import models
 
 
 class User(AbstractUser):
-    avatar = models.CharField(max_length=20, verbose_name='تصویر آواتار', null=True, blank=True)
+    avatar = models.ImageField(upload_to='images/profile', verbose_name='تصویر آواتار', null=True, blank=True)
     email_active_code = models.CharField(max_length=100, verbose_name='کد فعالسازی ایمیل')
+    about_user = models.TextField(null=True, blank=True, verbose_name='درباره شخص')
 
     class Meta:
         verbose_name = 'کاربر'
         verbose_name_plural = 'کاربران'
 
     def __str__(self):
-        if self.first_name is not '' and self.last_name is not '':
+        if self.first_name != '' and self.last_name != '':
             return self.get_full_name()
 
         return self.email
