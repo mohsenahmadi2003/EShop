a88d888cc1c896fbf148ad59712b0865711d3eb8
[Mohsen Ahmadi] <mohsenahmadi2003@yahoo.com>
2024-01-08 13:45:45 +0330

    [update]: changing context data article detail view, add article comment
    Co-authored-by: mhasanbash <mhasanbashogh@gmail.com>

--- article_module/views.py
+++ article_module/views.py
@@ -38,7 +38,7 @@ class ArticleDetailView(DetailView):
         article: Article = kwargs.get('object')
         context['comments'] = ArticleComment.objects.filter(article_id=article.id, parent=None).order_by(
             '-create_date').prefetch_related('articlecomment_set')
-
+        context['comments_count'] = ArticleComment.objects.filter(article_id=article.id).count()
         return context
 
 
@@ -57,8 +57,16 @@ def add_article_comment(request: HttpRequest):
         article_id = request.GET.get('article_id')
         article_comment = request.GET.get('article_comment')
         parent_id = request.GET.get('parent_id')
+        print(article_id, article_comment, parent_id)
         new_comment = ArticleComment(article_id=article_id, text=article_comment, user_id=request.user.id,
                                      parent_id=parent_id)
         new_comment.save()
+        context = {
+            'comments': ArticleComment.objects.filter(article_id=article_id, parent=None).order_by(
+                '-create_date').prefetch_related('articlecomment_set'),
+            'comments_count': ArticleComment.objects.filter(article_id=article_id).count()
+        }
+
+        return render(request, 'article_module/includes/article_comments_partial.html', context)
 
     return HttpResponse('response')
