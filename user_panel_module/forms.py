9745053e1200be44d6d3f46ad131e6ef70dde687
[Mohsen Ahmadi] <mohsenahmadi2003@yahoo.com>
2024-01-11 00:00:41 +0330

    [create]: add edit profile model form
    Co-authored-by: mhasanbash <mhasanbashogh@gmail.com>

---
+++ user_panel_module/forms.py
@@ -0,0 +1,31 @@
+from django import forms
+from account_module.models import User
+
+
+class EditProfileModelForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'avatar', 'address']
+        widgets = {
+            'first_name': forms.TextInput(attrs={
+                'class': 'form-control'
+            }),
+            'last_name': forms.TextInput(attrs={
+                'class': 'form-control'
+            }),
+            'avatar': forms.FileInput(attrs={
+                'class': 'form-control'
+            }),
+            'address': forms.Textarea(attrs={
+                'class': 'form-control',
+                'rows': 3,
+                'id': 'message'
+            })
+        }
+
+        labels = {
+            'first_name': 'نام',
+            'last_name': 'نام خانوادگی',
+            'avatar': 'تصویر پروفایل',
+            'address': 'آدرس'
+        }
