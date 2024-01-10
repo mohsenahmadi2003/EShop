336b7c48ffa43a869dc8f3611fae49aa82f45941
[Mohsen Ahmadi] <mohsenahmadi2003@yahoo.com>
2024-01-11 00:01:06 +0330

    [update]: add edit profile url
    Co-authored-by: mhasanbash <mhasanbashogh@gmail.com>

--- user_panel_module/urls.py
+++ user_panel_module/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.UserPanelDashboardView.as_view(), name='user_panel_dashboard')
+    path('', views.UserPanelDashboardView.as_view(), name='user_panel_dashboard'),
+    path('edit-profile', views.EditUserProfileView.as_view(), name='edit_profile_page'),
 ]
