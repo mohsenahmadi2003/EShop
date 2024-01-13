560040ad33aba6abcbe014f8d15bc4fec4529046
[Mohsen Ahmadi] <mohsenahmadi2003@yahoo.com>
2024-01-13 23:33:07 +0330

    [create]: add change password page view
    Co-authored-by: mhasanbash <mhasanbashogh@gmail.com>

--- user_panel_module/views.py
+++ user_panel_module/views.py
@@ -1,16 +1,18 @@
 from django.http import HttpRequest
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 from django.views import View
 from django.views.generic import TemplateView
 from account_module.models import User
-from .forms import EditProfileModelForm
+from .forms import EditProfileModelForm, ChangePasswordForm
+from django.contrib.auth import logout
 
 
-class UserPanelDashboardView(TemplateView):
+class UserPanelDashboardPage(TemplateView):
     template_name = 'user_panel_module/user_panel_dashboard_page.html'
 
 
-class EditUserProfileView(View):
+class EditUserProfilePage(View):
     def get(self, request: HttpRequest):
         current_user = User.objects.filter(id=request.user.id).first()
         edit_form = EditProfileModelForm(instance=current_user)
@@ -31,6 +33,31 @@ class EditUserProfileView(View):
             'current_user': current_user
         }
         return render(request, 'user_panel_module/edit_profile_page.html', context)
+
+
+class ChangePasswordPage(View):
+    def get(self, request: HttpRequest):
+        context = {
+            'form': ChangePasswordForm()
+        }
+        return render(request, 'user_panel_module/change_password_page.html', context)
+
+    def post(self, request: HttpRequest):
+        form = ChangePasswordForm(request.POST)
+        if form.is_valid():
+            current_user: User = User.objects.filter(id=request.user.id).first()
+            if current_user.check_password(form.cleaned_data.get('current_password')):
+                current_user.set_password(form.cleaned_data.get('password'))
+                current_user.save()
+                logout(request)
+                return redirect(reverse('login_page'))
+            else:
+                form.add_error('password', 'کلمه عبور وارد شده اشتباه می باشد')
+
+        context = {
+            'form': form
+        }
+        return render(request, 'user_panel_module/change_password_page.html', context)
 
 
 def user_panel_menu_component(request: HttpRequest):
