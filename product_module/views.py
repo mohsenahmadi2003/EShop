a4188654c31edba84102a6402b3a1f045efb4434
[Mohsen Ahmadi] <mohsenahmadi2003@yahoo.com>
2024-02-06 19:35:39 +0330

    [update]: change price range

--- product_module/views.py
+++ product_module/views.py
@@ -1,8 +1,9 @@
+from django.db.models import Count
 from django.http import HttpRequest
 from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView
 from django.views.generic.base import View
-from .models import Product, ProductCategory
+from .models import Product, ProductCategory, ProductBrand
 
 
 class ProductListView(ListView):
@@ -12,9 +13,34 @@ class ProductListView(ListView):
     ordering = ['-price']
     paginate_by = 6
 
+    def get_context_data(self, *, object_list=None, **kwargs):
+        print('context_data')
+        context = super(ProductListView, self).get_context_data()
+        query = Product.objects.all()
+        product: Product = query.order_by('-price').first()
+        db_max_price = product.price if product is not None else 0
+        context['db_max_price'] = db_max_price
+        context['start_price'] = self.request.GET.get('start_price') or 0
+        context['end_price'] = self.request.GET.get('end_price') or db_max_price
+        return context
+
     def get_queryset(self):
+        print('query_set')
         query = super(ProductListView, self).get_queryset()
         category_name = self.kwargs.get('cat')
+        brand_name = self.kwargs.get('brand')
+        request: HttpRequest = self.request
+        start_price = request.GET.get('start_price')
+        end_price = request.GET.get('end_price')
+        if start_price is not None:
+            query = query.filter(price__gte=start_price)
+
+        if end_price is not None:
+            query = query.filter(price__lte=end_price)
+
+        if brand_name is not None:
+            query = query.filter(brand__url_title__iexact=brand_name)
+
         if category_name is not None:
             query = query.filter(category__url_title__iexact=category_name)
         return query
@@ -47,3 +73,11 @@ def product_categories_component(request: HttpRequest):
         'categories': product_categories
     }
     return render(request, 'product_module/components/product_categories_component.html', context)
+
+
+def product_brands_component(request: HttpRequest):
+    product_brands = ProductBrand.objects.annotate(products_count=Count('product')).filter(is_active=True)
+    context = {
+        'brands': product_brands
+    }
+    return render(request, 'product_module/components/product_brands_component.html', context)
