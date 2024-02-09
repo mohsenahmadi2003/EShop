f60cd39b6107f01081f9151ce04633cb0948350a
[Mohsen Ahmadi] <mohsenahmadi2003@yahoo.com>
2024-02-09 18:16:02 +0330

    [update]: add settings for local and production mode

--- manage.py
+++ manage.py
@@ -2,11 +2,15 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from EShop.settings.base import DEBUG
 
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EShop.settings')
+    if DEBUG:
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EShop.settings.local')
+    else:
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EShop.settings.production')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
