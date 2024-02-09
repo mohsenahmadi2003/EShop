2fe73d24ec5ce4483f6d32313e5505bb4c47db95
[Mohsen Ahmadi] <mohsenahmadi2003@yahoo.com>
2024-02-09 18:16:39 +0330

    [create]: add local mode settings

---
+++ EShop/settings/local.py
@@ -0,0 +1,12 @@
+from .base import *
+
+# Database
+# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+ALLOWED_HOSTS = ['*']
