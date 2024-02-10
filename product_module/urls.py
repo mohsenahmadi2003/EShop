dd2081a0b451bd0501a9a1bd4671d2460cf8ffc9
[Mohsen Ahmadi] <mohsenahmadi2003@yahoo.com>
2024-02-10 16:18:56 +0330

    [update]: change product detail url

--- product_module/urls.py
+++ product_module/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
@@ -6,5 +6,7 @@ urlpatterns = [
     path('cat/<cat>', views.ProductListView.as_view(), name='product-categories-list'),
     path('brand/<brand>', views.ProductListView.as_view(), name='product-list-by-brands'),
     path('product-favorite', views.AddProductFavorite.as_view(), name='product-favorite'),
-    path('<slug:slug>', views.ProductDetailView.as_view(), name='product-detail'),
+    # path('<slug:slug>', views.ProductDetailView.as_view(), name='product-detail'),
+    # path('<slug:slug>', views.ProductDetailView.as_view(), name='product-detail'),
+    re_path(r'(?P<slug>[^/]+)/?$', views.ProductDetailView.as_view(), name='product-detail'),
 ]
