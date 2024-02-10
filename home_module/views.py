a792e0220497319b3e47520bd100fe43a47d2cde
[Mohsen Ahmadi] <mohsenahmadi2003@yahoo.com>
2024-02-10 16:18:30 +0330

    [update]: add most visit products in HomeView

--- home_module/views.py
+++ home_module/views.py
@@ -1,6 +1,6 @@
+from django.db.models import Count
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
-
 from product_module.models import Product
 from site_module.models import SiteSetting, FooterLinkBox, Slider
 from utils.convertors import group_list
@@ -14,7 +14,10 @@ class HomeView(TemplateView):
         sliders = Slider.objects.filter(is_active=True)
         context['sliders'] = sliders
         latest_products = Product.objects.filter(is_active=True, is_delete=False).order_by('-id')[:12]
+        most_visit_products = Product.objects.filter(is_active=True, is_delete=False).annotate(
+            visit_count=Count('productvisit')).order_by('-visit_count')[:12]
         context['latest_products'] = group_list(latest_products)
+        context['most_visit_products'] = group_list(most_visit_products)
         return context
 
 
