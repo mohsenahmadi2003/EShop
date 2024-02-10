74b6f25e6c8dadea03d01e51c2ed75434b5f48dd
[Mohsen Ahmadi] <mohsenahmadi2003@yahoo.com>
2024-02-10 16:15:38 +0330

    [update]: change slug filed in Product model

--- product_module/models.py
+++ product_module/models.py
@@ -1,9 +1,8 @@
 from django.db import models
 from django.urls import reverse
 from account_module.models import User
+from django.utils.text import slugify
 
-
-# Create your models here.
 
 class ProductCategory(models.Model):
     title = models.CharField(max_length=300, db_index=True, verbose_name='عنوان')
@@ -40,7 +39,8 @@ class Product(models.Model):
     price = models.IntegerField(verbose_name='قیمت')
     short_description = models.CharField(max_length=360, db_index=True, null=True, verbose_name='توضیحات کوتاه')
     description = models.TextField(verbose_name='توضیحات اصلی', db_index=True)
-    slug = models.SlugField(default="", null=False, db_index=True, blank=True, max_length=200, unique=True, verbose_name='عنوان در url')
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True,
+                            db_index=True, blank=True, default="", verbose_name='عنوان در url')
     is_active = models.BooleanField(default=False, verbose_name='فعال / غیرفعال')
     is_delete = models.BooleanField(verbose_name='حذف شده / نشده')
 
@@ -48,7 +48,8 @@ class Product(models.Model):
         return reverse('product-detail', args=[self.slug])
 
     def save(self, *args, **kwargs):
-        # self.slug = slugify(self.title)
+        if not self.slug:
+            self.slug = slugify(self.title.strip())
         super().save(*args, **kwargs)
 
     def __str__(self):
