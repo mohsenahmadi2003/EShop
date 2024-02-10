4355167e5eea6860b40be31333cbba40f0378a32
[Mohsen Ahmadi] <mohsenahmadi2003@yahoo.com>
2024-02-10 12:24:45 +0330

    [update]: register ProductVisit model

--- product_module/admin.py
+++ product_module/admin.py
@@ -1,9 +1,6 @@
 from django.contrib import admin
 from . import models
 
-
-# Register your models here.
-
 class ProductAdmin(admin.ModelAdmin):
     list_filter = ['category', 'is_active']
     list_display = ['title', 'price', 'is_active', 'is_delete']
@@ -14,3 +11,4 @@ admin.site.register(models.Product, ProductAdmin)
 admin.site.register(models.ProductCategory)
 admin.site.register(models.ProductTag)
 admin.site.register(models.ProductBrand)
+admin.site.register(models.ProductVisit)
