89ba3a9b471b5819535768723c980d4b63283703
[Mohsen Ahmadi] <mohsenahmadi2003@yahoo.com>
2024-02-13 12:29:46 +0330

    [create]: order model

--- order_module/models.py
+++ order_module/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from account_module.models import User
+from product_module.models import Product
 
-# Create your models here.
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='کاربر')
+    is_paid = models.BooleanField(verbose_name='نهایی شده/نشده')
+    payment_date = models.DateField(null=True, blank=True, verbose_name='تاریخ پرداخت')
+
+    def __str__(self):
+        return str(self.user)
+
+    class Meta:
+        verbose_name = 'سبد خرید'
+        verbose_name_plural = 'سبدهای خرید کاربران'
