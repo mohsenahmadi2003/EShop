2b1bd1ad7079c282280a3791818e08a672abc9b2
[Mohsen Ahmadi] <mohsenahmadi2003@yahoo.com>
2024-02-13 12:27:47 +0330

    [create]: add order module

---
+++ EShop/settings.py
@@ -0,0 +1,165 @@
+from pathlib import Path
+import environ
+
+# Initialize environ
+env = environ.Env()
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
+# Read environment variables from the .env file
+environ.Env.read_env()
+
+# SECURITY WARNING: keep the secret key used in production secret!
+
+# Retrieve the SECRET_KEY from environment variables
+SECRET_KEY = env('SECRET_KEY')
+
+# Retrieve the DEBUG value from environment variables with a default value if not defined
+DEBUG = env('DEBUG')
+
+ALLOWED_HOSTS = ['*']
+
+# Application definition
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    # internal apps
+    'account_module',
+    'home_module',
+    'product_module',
+    'contact_module',
+    'site_module',
+    'article_module',
+    'polls',
+    'user_panel_module',
+    'order_module',
+
+    # external apps
+    'django_render_partial',
+    'sorl.thumbnail',
+    'jalali_date',
+]
+
+# Middleware classes
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+# Root URL configuration module
+ROOT_URLCONF = 'EShop.urls'
+
+# Template configuration
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / 'templates'],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+# WSGI application
+WSGI_APPLICATION = 'EShop.wsgi.application'
+
+# Database configuration (SQLite in this case)
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+# Custom user model
+AUTH_USER_MODEL = 'account_module.User'
+
+# Password validation settings
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+# Language code
+LANGUAGE_CODE = 'en-us'
+
+# Time zone
+TIME_ZONE = 'UTC'
+
+# Internationalization settings
+USE_I18N = True
+
+# Time zone support
+USE_TZ = True
+
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = 'static/'
+
+# Media files
+MEDIA_ROOT = BASE_DIR / 'uploads'
+MEDIA_URL = '/medias/'
+
+# Additional static files directories
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+
+# Default primary key field type
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Session cookie age (in seconds)
+SESSION_COOKIE_AGE = 1814400
+
+# Email settings (Gmail SMTP in this case)
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_PORT = 587
+
+# Default settings for Jalali date (optional)
+JALALI_DATE_DEFAULTS = {
+    'LIST_DISPLAY_AUTO_CONVERT': False,
+    'Strftime': {
+        'date': '%y/%m/%d',
+        'datetime': '%H:%M:%S _ %y/%m/%d',
+    },
+    'Static': {
+        'js': [
+            'admin/js/django_jalali.min.js',
+        ],
+        'css': {
+            'all': [
+                'admin/jquery.ui.datepicker.jalali/themes/base/jquery-ui.min.css',
+            ]
+        }
+    },
+}
