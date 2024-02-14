15b20058e1090e46d0f62e0d37146c30e8d0a929
[Mohsen Ahmadi] <mohsenahmadi2003@yahoo.com>
2024-02-14 11:22:51 +0330

    [update]: create add_product_to_order view

--- order_module/views.py
+++ order_module/views.py
@@ -1,3 +1,19 @@
+from django.http import HttpRequest, HttpResponse, JsonResponse
 from django.shortcuts import render
+from product_module.models import Product
 
-# Create your views here.
+
+def add_product_to_order(request: HttpRequest):
+    product_id = request.GET.get('product_id')
+    count = request.GET.get('count')
+
+    if request.user.is_authenticated:
+        product = Product.objects.filter(id=product_id, is_active=True, is_delete=False).first()
+        if product is not None:
+            # get current user open order
+            # add product to order
+            pass
+    else:
+        return JsonResponse({
+            'status': 'not_auth'
+        })
