d68131c2635924297a3d3d3c10af1b419b1858b7
[Mohsen Ahmadi] <mohsenahmadi2003@yahoo.com>
2024-02-14 18:17:32 +0330

    [update]: Implementation of the product purchase section

--- order_module/views.py
+++ order_module/views.py
@@ -1,6 +1,6 @@
-from django.http import HttpRequest, HttpResponse, JsonResponse
-from django.shortcuts import render
+from django.http import HttpRequest, JsonResponse
 from product_module.models import Product
+from .models import Order, OrderDetail
 
 
 def add_product_to_order(request: HttpRequest):
@@ -10,9 +10,22 @@ def add_product_to_order(request: HttpRequest):
     if request.user.is_authenticated:
         product = Product.objects.filter(id=product_id, is_active=True, is_delete=False).first()
         if product is not None:
-            # get current user open order
-            # add product to order
-            pass
+            current_order, created = Order.objects.get_or_create(is_paid=False, user_id=request.user.id)
+            current_order_detail = current_order.orderdetail_set.filter(product_id=product_id).first()
+            if current_order_detail is not None:
+                current_order_detail.count += int(count)
+                current_order_detail.save()
+            else:
+                new_detail = OrderDetail(order_id=current_order.id, product_id=product_id, count=count)
+                new_detail.save()
+
+            return JsonResponse({
+                'status': 'success'
+            })
+        else:
+            return JsonResponse({
+                'status': 'not_found'
+            })
     else:
         return JsonResponse({
             'status': 'not_auth'
