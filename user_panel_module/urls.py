b125612150fa3ff00dba8737b0b765ba8c1d3d06
[Mohsen Ahmadi] <mohsenahmadi2003@yahoo.com>
2024-02-15 11:25:16 +0330

    [update]: add user-basket url

--- user_panel_module/urls.py
+++ user_panel_module/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.UserPanelDashboardPage.as_view(), name='user_panel_dashboard'),
     path('change-pass', views.ChangePasswordPage.as_view(), name='change_password_page'),
     path('edit-profile', views.EditUserProfilePage.as_view(), name='edit_profile_page'),
+    path('user-basket', views.user_basket, name='user_basket_page'),
 ]
