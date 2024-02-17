cf02943fb0219fb6f0fe2cd7695d4e565a36f217
[Mohsen Ahmadi] <mohsenahmadi2003@yahoo.com>
2024-02-17 15:35:18 +0330

    [update]: add calculate_total_price method

--- order_module/models.py
+++ order_module/models.py
@@ -8,6 +8,17 @@ class Order(models.Model):
     is_paid = models.BooleanField(verbose_name='نهایی شده/نشده')
     payment_date = models.DateField(null=True, blank=True, verbose_name='تاریخ پرداخت')
 
+    def calculate_total_price(self):
+        total_amount = 0
+        if self.is_paid:
+            for order_detail in self.orderdetail_set.all():
+                total_amount += order_detail.final_price * order_detail.count
+        else:
+            for order_detail in self.orderdetail_set.all():
+                total_amount += order_detail.product.price * order_detail.count
+
+        return total_amount
+
     def __str__(self):
         return str(self.user)
 
