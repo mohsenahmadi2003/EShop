91b58e27a7177f3e81d4ecf5e3663f03549ce0d4
[Mohsen Ahmadi] <mohsenahmadi2003@yahoo.com>
2024-02-23 22:10:23 +0330

    [update]: implementation of Zarin Pal portal method

--- order_module/views.py
+++ order_module/views.py
@@ -1,21 +1,37 @@
-from django.http import HttpRequest, JsonResponse
+from django.http import HttpRequest, JsonResponse, HttpResponse
 from product_module.models import Product
 from .models import Order, OrderDetail
-from utils.validation import validate_count
+from django.shortcuts import redirect
+import requests
+import json
+
+# Create your views here.
+
+
+MERCHANT = 'XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX'
+ZP_API_REQUEST = "https://api.zarinpal.com/pg/v4/payment/request.json"
+ZP_API_VERIFY = "https://api.zarinpal.com/pg/v4/payment/verify.json"
+ZP_API_STARTPAY = "https://www.zarinpal.com/pg/StartPay/{authority}"
+amount = 11000  # Rial / Required
+description = "نهایی کردن خرید شما از سایت ما"  # Required
+email = ''  # Optional
+mobile = ''  # Optional
+# Important: need to edit for realy server.
+CallbackURL = 'http://localhost:8000/order/verify-payment/'
 
 
 def add_product_to_order(request: HttpRequest):
     product_id = int(request.GET.get('product_id'))
-
-    count = request.GET.get('count')
-    if not validate_count(count):
+    count = int(request.GET.get('count'))
+    if count < 1:
+        # count = 1
         return JsonResponse({
             'status': 'invalid_count',
-            'text': 'مقدار وارد شده معتبر نمی‌باشد',
+            'text': 'مقدار وارد شده معتبر نمی باشد',
             'confirm_button_text': 'مرسی از شما',
             'icon': 'warning'
         })
-    count = int(count)
+
     if request.user.is_authenticated:
         product = Product.objects.filter(id=product_id, is_active=True, is_delete=False).first()
         if product is not None:
@@ -48,3 +64,54 @@ def add_product_to_order(request: HttpRequest):
             'confirm_button_text': 'ورود به سایت',
             'icon': 'error'
         })
+
+
+def request_payment(request: HttpRequest):
+    req_data = {
+        "merchant_id": MERCHANT,
+        "amount": amount,
+        "callback_url": CallbackURL,
+        "description": description,
+        "metadata": {"mobile": mobile, "email": email}
+    }
+    req_header = {"accept": "application/json", "content-type": "application/json'"}
+    req = requests.post(url=ZP_API_REQUEST, data=json.dumps(req_data), headers=req_header)
+    authority = req.json()['data']['authority']
+    if len(req.json()['errors']) == 0:
+        return redirect(ZP_API_STARTPAY.format(authority=authority))
+    else:
+        e_code = req.json()['errors']['code']
+        e_message = req.json()['errors']['message']
+        return HttpResponse(f"Error code: {e_code}, Error Message: {e_message}")
+
+
+def verify_payment(request: HttpRequest):
+    t_authority = request.GET['Authority']
+    if request.GET.get('Status') == 'OK':
+        req_header = {"accept": "application/json", "content-type": "application/json'"}
+        req_data = {
+            "merchant_id": MERCHANT,
+            "amount": amount,
+            "authority": t_authority
+        }
+        req = requests.post(url=ZP_API_VERIFY, data=json.dumps(req_data), headers=req_header)
+        if len(req.json()['errors']) == 0:
+            t_status = req.json()['data']['code']
+            if t_status == 100:
+                return HttpResponse('Transaction success.\nRefID: ' + str(
+                    req.json()['data']['ref_id']
+                ))
+            elif t_status == 101:
+                return HttpResponse('Transaction submitted : ' + str(
+                    req.json()['data']['message']
+                ))
+            else:
+                return HttpResponse('Transaction failed.\nStatus: ' + str(
+                    req.json()['data']['message']
+                ))
+        else:
+            e_code = req.json()['errors']['code']
+            e_message = req.json()['errors']['message']
+            return HttpResponse(f"Error code: {e_code}, Error Message: {e_message}")
+    else:
+        return HttpResponse('Transaction failed or canceled by user')
