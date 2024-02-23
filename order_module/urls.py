f66c7e4f43af5db1e8669aa9d7da5090f5f37b70
[Mohsen Ahmadi] <mohsenahmadi2003@yahoo.com>
2024-02-23 22:11:50 +0330

    [update]: add request-payment and verify-payment url

--- order_module/urls.py
+++ order_module/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('add-to-order', views.add_product_to_order, name='add_product_to_order')
+    path('add-to-order', views.add_product_to_order, name='add_product_to_order'),
+    path('request-payment', views.request_payment, name='request_payment'),
+    path('verify-payment', views.verify_payment, name='verify_payment')
 ]
