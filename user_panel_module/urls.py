77641bca7e3ae0477b2ccd84ce9bf8f3e47b5b5e
[Mohsen Ahmadi] <mohsenahmadi2003@yahoo.com>
2024-03-01 22:20:30 +0330

    [update]: add my-shopping-detail url

--- user_panel_module/urls.py
+++ user_panel_module/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('edit-profile', views.EditUserProfilePage.as_view(), name='edit_profile_page'),
     path('user-basket', views.user_basket, name='user_basket_page'),
     path('my-shopping', views.MyShopping.as_view(), name='user_shopping_page'),
+    path('my-shopping-detail/<order_id>', views.my_shopping_detail, name='user_shopping_detail_page'),
     path('remove-order-detail', views.remove_order_detail, name='remove_order_detail_ajax'),
     path('change-order-detail', views.change_order_detail_count, name='change_order_detail_count_ajax'),
 ]
