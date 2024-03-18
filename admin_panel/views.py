1a1e460b89ef81dace952f97fdd0b40efeaf9c1d
[Mohsen Ahmadi] <mohsenahmadi2003@yahoo.com>
2024-03-18 22:14:22 +0330

    [update]: add ArticleEditView

--- admin_panel/views.py
+++ admin_panel/views.py
@@ -1,17 +1,18 @@
-from django.http import HttpRequest, HttpResponse
+from django.http import HttpRequest
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.urls import reverse, reverse_lazy
+from django.views.generic import ListView, UpdateView
 from article_module.models import Article
 
 
-def all_articles(request: HttpRequest):
-    return render(request, 'admin_panel/articles_list.html')
+def index(request: HttpRequest):
+    return render(request, 'admin_panel/home/index.html')
 
 
 class ArticlesListView(ListView):
     model = Article
     paginate_by = 12
-    template_name = 'admin_panel/articles_list.html'
+    template_name = 'admin_panel/articles/articles_list.html'
 
     def get_context_data(self, *args, **kwargs):
         context = super(ArticlesListView, self).get_context_data(*args, **kwargs)
@@ -23,3 +24,11 @@ class ArticlesListView(ListView):
         if category_name is not None:
             query = query.filter(selected_categories__url_title__iexact=category_name)
         return query
+
+
+class ArticleEditView(UpdateView):
+    model = Article
+    template_name = 'admin_panel/articles/edit_article.html'
+    fields = '__all__'
+    success_url = reverse_lazy('admin_articles')
+    # success_url = '/admin-panel/articles'
