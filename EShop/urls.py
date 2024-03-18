5519ee8d5a3f99ff2d8e18592a5ae495793022df
[Mohsen Ahmadi] <mohsenahmadi2003@yahoo.com>
2024-03-18 19:56:51 +0330

    [update]: add admin panel url

--- EShop/urls.py
+++ EShop/urls.py
@@ -11,7 +11,9 @@ urlpatterns = [
     path('products/', include('product_module.urls')),
     path('user/', include('user_panel_module.urls')),
     path('order/', include('order_module.urls')),
+    path('admin-panel/', include('admin_panel.urls')),
     path('admin/', admin.site.urls),
+
 ]
 
 urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
