b11bdf439ccfa1363e26525d3dd8b05b817b1bce
[Mohsen Ahmadi] <mohsenahmadi2003@yahoo.com>
2024-03-18 22:14:01 +0330

    [update]: change path

--- admin_panel/urls.py
+++ admin_panel/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('dashboard/', views.ArticlesListView.as_view(), name='admin_articles'),
+    path('', views.index, name='admin_dashboard'),
+    path('articles/', views.ArticlesListView.as_view(), name='admin_articles'),
+    path('articles/edit/<pk>', views.ArticleEditView.as_view(), name='admin_edit_article'),
 ]
